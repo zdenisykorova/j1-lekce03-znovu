371ef09116d13fc5fc8847262b04c46d804612b8
zdeni sykorova <sykorova@hotmail.com>
2025-04-22 13:35:08 +0200

    trojúhelník

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,10 +9,20 @@ public class HlavniProgram {
         new HlavniProgram().start();
     }
 
+    public void nakresliTrojuhelnik(int delkaStranyA, int delkaStranyB, int delkaStranyC) {
+        zofka.move(delkaStranyA);
+        zofka.turnRight(90);
+        zofka.move(delkaStranyB);
+        zofka.turnRight(135);
+        zofka.move(delkaStranyC);
+    }
+
     public void start() {
         zofka = new Turtle();
 
         //TODO Tady bude kód pro kreslení želví grafiky.
+        nakresliTrojuhelnik(40,50,60);
+        nakresliTrojuhelnik(80, 100,130);
     }
 
 }
